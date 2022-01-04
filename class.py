de1e8e2fb85f6824ae91778d9d1ab1b89326ea8c
GARCl-A <lucasgarcia@poli.ufrj.br>
2022-01-04 14:59:53 -0300

    Estrutura das classes + Listas
    -raças, alinhamento, classes, ocupações e riquezas
    
    -definições de classes pacificas e vilões (funções NÃO implementadas)

---
+++ class.py
@@ -0,0 +1,87 @@
+class NPC():
+    def __init__(self):
+        self._nome = None
+        self._raça = None
+        self._alinhamento = None
+        self._caracteristicas_fisicas = None
+        self._caracteristicas_pessoais = None
+        self._classe = None
+        self._riqueza = None
+
+    def set_nome(self,nome):
+        pass
+    
+    def set_raça(self,raça):
+        pass
+
+    def set_alinhamento(self,alinhamento):
+        pass
+
+    def set_caracteristicas_fisicas(self,caracteristicas_fisicas):
+        pass
+
+    def set_caracteristicas_pessoais(self,caracteristicas_pessoais):
+        pass
+
+    def set_classe(self,classe):
+        pass
+
+    def set_riqueza(self,riqueza):
+        pass
+
+    def gera_nome(self):
+        pass
+    
+    def gera_raça(self):
+        pass
+
+    def gera_alinhamento(self):
+        pass
+
+    def gera_caracteristicas_fisicas(self):
+        pass
+
+    def gera_caracteristicas_pessoais(self):
+        pass
+
+    def gera_classe(self):
+        pass
+
+    def gera_riqueza(self):
+        pass
+
+class Pacifico(NPC):
+    def __init__(self):
+        super().__init__()
+        self._ocupaçao = None
+
+    def set_ocupacao(self,ocupacao):
+        pass
+
+    def gera_ocupacao(self):
+        pass
+
+class Vilao(NPC):
+    def __init__(self):
+        super().__init__()
+        self._motivacao = None
+        self._influencia = None
+        self._poderes = None
+    
+    def set_motivacao(self,motivacao):
+        pass
+
+    def set_influencia(self,influencia):
+        pass
+
+    def set_poderes(self,poderes):
+        pass
+
+    def gera_motivacao(self):
+        pass
+
+    def gera_influencia(self):
+        pass
+
+    def gera_poderes(self):
+        pass
---
+++ listas.py
@@ -0,0 +1,16 @@
+lista_raças = ["Kobold", "Orc", "Leshy", "Lizardfolk", "Sprite", "Humano", "Anão", "Elfo", "Gnomo", "Goblin", "Halfling", 
+"Gnoll", "Gigante", "Ikidaris", "Tiefling", "Aasimar", "Kenku", "Aarakocra", "Draconato", "Meio-Elfo", "Meio-Orc"
+"Golias", "Centauros", "Forjados", "Golias", "Genasi"]
+
+lista_alinhamento_CO = ["Caótico", "Neutro", "Leal"]
+lista_alinhamento_BM = ["Bom", "Neutro", "Mal"]
+
+classes_jog = ["Artíficie", "Bárbaro", "Bardo", "Clérigo", "Druida", "Feiticeiro","Guerreiro","Paladino","Bruxo", 
+"Ladino", "Mago", "Monge", "Patrulheiro"]
+
+classes_npc = ["Plebeu", "Nobre", "Especialista", "Combatente", "Acólito"]
+
+riquezas = ["Abaixo da pobreza", "Pobre", "Média baixa", "Média", "Média alta", "Rico", "Muito Rico"]
+
+ocupaçoes = ["Camponês", "Lenhador", "Ferreiro", "Pastor", "Carniceiro", "Cirieiro", "Pergaminheiro", "Ourives", "Vidreiro", 
+"Canteiro", "Vendedor", "Boticário", "Alfaiate", "Bancário", "Soldado", "Taverneiro", "Músico", "Escrivão"]
